e921839a3624e96735f2201b08ed532cfed5e42f
oopyqoo <69220879+yaoayh@users.noreply.github.com>
2021-07-02 00:46:48 +0200

    start version beta 1.0
    prabably working version beta with several slogans,
    Changing from last alpha version:
    -adding slogan scripts
    Need to add:
    -left td "whoo" image
    -left td "wow" image

--- hello/slogans/script.js
+++ hello/slogans/script.js
@@ -7,33 +7,63 @@ var imageHal2  = imageHal.addEventListener("touchstart", function(){
 var imageHalReturn = imageHal.addEventListener("touchend", function(){
     imageHal.src = "pictures/hal/halRed.gif";
     newSlogan();    
-    console.log(m);
-    console.log(s);
+    numberOfProducts ++;
+    prise = prise + 0.50;
 });
-var m = 0;
-var s = 0;
+var numberOfTr = 0;
+var numberOfTd = -2;
+var numberOfProducts = 1;
+var prise = 0.50;
 function newSlogan(){
-    for(x = 0; x < 3; x++){
+    var tableElement = document.getElementsByTagName("table")[0];
     var createTr = document.createElement("tr");
-    var createTd = document.createElement("td");
-    var tableElement = document.querySelector("table");
-
     tableElement.appendChild(createTr);
-    // var createTrTd = createTr.appendChild(createTd);
-    var trNumber = document.querySelector("tr");
-    m = m+1;
-    for(i = 0; i < 3; i++){
-        trNumber.appendChild(createTd);
-        
-        // document.tr.appendChild(createTd);
-        // var createTdId = document.getElementsByTagName("td")[i];
-        
-        // // createTh.createTextNode(textNode);
-        console.log("how it's work x3");
-        s = s + 1;
-        // return s;
-    }
+
+    for(x = 0; x < 3; x++){
+        var trElement = document.getElementsByTagName("tr")[numberOfTr];
+        var createTd = document.createElement("td");
+        trElement.appendChild(createTd);
+        numberOfTd ++;
+        }
+
+    var tdCreate = document.getElementsByTagName("td")[numberOfTd];
+    var createTdSlogan = document.createTextNode(newSloganFunction());
+    tdCreate.appendChild(createTdSlogan);
+    numberOfTr ++;
     console.log("check");
-    // return m;
-}
-}
+    document.getElementById("itemsNumber").innerHTML = "Ilość: " + numberOfProducts;
+    document.getElementById("amount").innerHTML = "Do zapłaty: " + prise + "$";
+    console.log(numberOfTr);
+    console.log(numberOfTd);
+ }
+
+// x = addEventListener("click", function(){
+//     newP = document.createElement("p");
+//     newNodeP = document.createTextNode(newSloganFunction());
+//     newP.appendChild(newNodeP);
+//     tableX = document.querySelector("table");
+//     tableX.appendChild(newP);
+// });
+
+
+
+
+// script for creating new slogans
+var word1 = ["interpersonalne", "asymetryczne", "empiryczne", "strategiczne", "liryczne", "kanoniczne", "demokratyczne", "nieskalowalne", "jednokierunkowe", "oportunistyczne"];
+var word2 = ["wyładowanie", "wyprofilowanie", "udokumentowanie", "wyrównanie", "wyodrębnienie", "rozgałęzienie", "oderwanie", "porównanie", "reagowanie", "korelacjonowanie"];
+var word3 = ["asocjalności twórczej", "pragmatyzmu społecznego", "liberalności narodowej", "jedności komunistycznej", "niepoprawności politycznej", "tradycji Polskiej", "reakcji obronnej", "prokrasycynacji", "teorii darwina", "anonimowości społecznej"];
+function getRandom1() {
+    return Math.floor(Math.random() * word1.length);
+  }
+
+function getRandom2() {
+   return Math.floor(Math.random() * word2.length);
+ }
+
+ function getRandom3() {
+    return Math.floor(Math.random() * word3.length);
+  }
+function newSloganFunction(){
+newSloganX = '"' + word1[getRandom1()] + " " + word2[getRandom2()] + " " + word3[getRandom3()] + '"';
+return newSloganX;}
+// alert(newSlogan);
